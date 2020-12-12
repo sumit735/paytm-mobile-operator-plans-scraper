29534b9b672567af5266d794712fe3254f759818
sumit735 <sumitkumardash1999@gmail.com>
2020-12-12 09:53:07 +0530

    method change

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const app = express();
 const axios = require('axios');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
+app.use(cors())
 
 app.get('/test', (req, res) => {
     res.send('hello');
 })
 
-app.get('/', async (req, res) => {
+app.post('/', async (req, res) => {
     const { mobile } = req.body;
     console.log(mobile);
     axios.get(`https://digitalapiproxy.paytm.com/v1/mobile/getopcirclebyrange?channel=web&version=2&number=${mobile}&child_site_id=1&site_id=1&locale=en-in`)
