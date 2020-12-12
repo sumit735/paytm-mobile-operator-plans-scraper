185a503c7e706c4d53f28cc5ed15e963edbf7a28
sumit735 <sumitkumardash1999@gmail.com>
2020-12-12 08:57:36 +0530

    added bodyparser

--- app.js
+++ app.js
@@ -6,18 +6,18 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 app.get('/', async (req, res) => {
-    const { mobile } = req.body.mobile;
+    const { mobile } = req.body;
     console.log(mobile);
-    axios.get('https://digitalapiproxy.paytm.com/v1/mobile/getopcirclebyrange?channel=web&version=2&number=9853174100&child_site_id=1&site_id=1&locale=en-in')
+    axios.get(`https://digitalapiproxy.paytm.com/v1/mobile/getopcirclebyrange?channel=web&version=2&number=${mobile}&child_site_id=1&site_id=1&locale=en-in`)
     .then(function (response) {
         // handle success
-        console.log(response);
+        console.log(response.data);
         res.json(response.data)
     })
     .catch(function (error) {
         // handle error
-        console.log(error);
-        res.json(error)
+        console.log('inside error');
+        res.status(500).json({message: "failed to fetch data"});
     })
 });
 
